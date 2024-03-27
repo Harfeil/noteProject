88f6cb47a248c69f7871f0e8a40f5f426ae5d44b
Harfeil <harfeilsalmeron1@gmail.com>
2024-03-27 14:32:50 +0800

    updated login

--- frontend/components/loginPage.php
+++ frontend/components/loginPage.php
@@ -1,25 +1,80 @@
-
 <?php
+include "templates/header.php";
+require_once('db_connector.php');
 
-    include "templates/header.php";
+$emailError = "";
+$passwordError = "";
+$checkError = "";
+$email = "";
+$password = "";
+$messageError = "";
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $check = isset($_POST["checkme"]);
+    $error = false;
+
+    if(empty($email)){
+        $error = true;
+        $emailError = "Email must be inputted <br>";
+    } else {
+        $error = false;
+    }
+
+    if(empty($password)){
+        $error = true;
+        $passwordError = "Password must be inputted <br>";
+    } else {
+        $error = false;
+    }
+
+    if(empty($check)){
+        $error = true;
+        $checkError = "Check the checkbox. <br>";
+    } else {
+        $error = false;
+    }
+
+    if($error === false) {
+        $sql = "SELECT user_password FROM users WHERE user_email = '$email'";
+        $result = $connection->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $hashedpassword = $row["user_password"];
+            echo $password;
+            if(password_verify($password, $row["user_password"])) {
+                header("Location: maindash.php");
+            } else {
+                $messageError = "Incorrect Password";
+            }
+        } else {
+            $messageError = "User not found";
+        }
+    }
+}
 ?>
-<link rel="stylesheet" href="styles/log.css">
+<link rel="stylesheet" href="styles/myLogin.css">
 
 <div class = "loginContainer">
     <div class = "loginTitle">
         <h1 id = "loginTitle">Note<span id = "spanLogo">It!</span></h1>
     </div>
     <div class = "formContainer">
-        <form action="">
-            <label for="">Username</label><br><br>
-            <input class = "nameText" type="text"><br><br>
+        <form action="" method = "post">
+            <label for="">Email:</label><br><br>
+            <input class = "nameText" type="text" name = "email" value = <?php echo $email ?>><br><br>
+            <label for="" id = "emailError"><?php echo $emailError ?></label><br>
             <label for="">Password</label><br><br>
-            <input class = "passwordText"type="text" ><br><br>
-            <input class = "checkbox" type="checkbox">
+            <input class = "passwordText"type="text" name = "password"  value = <?php echo $password ?>><br><br>
+            <label for="" id = "passwordError"><?php echo $passwordError ?></label><br>
+            <input id = "checkbox" type="checkbox" name = "checkme">
             <label for="">Sign Me In</label>
             <a class = "forgotPass" href="">Forgot Password?</a><br>
+            <label for="" id = "checkError"><?php echo $checkError ?></label>
             <button class = "signInBtn">SIGN IN</button>
+            <label for="" id = "passwordError"><?php echo $messageError ?></label>
         </form>
     </div>
 </div>
--- frontend/components/register.php
+++ frontend/components/register.php
@@ -1,39 +1,163 @@
 <?php
 
     include "templates/header.php";
+    require_once('db_connector.php');
+    
+    $errorFname = "";
+    $errorLname = "";
+    $errorAge = "";
+    $errorGender = "";
+    $errorEmail = "";
+    $errorPass = "";
+    $errorCpass = "";
+    $fname = "";
+    $lname = "";
+    $age = "";
+    $gender = "";
+    $email = "";
+    $password = "";
+    $cpassword = "";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $age = $_POST["age"];
+        $gender = isset($_POST["Gender"]);
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $cpassword = $_POST["cpassword"];
+        $error = false;
+
+        if (!preg_match("/^[a-zA-Z]+$/", $fname)) {
+            $error = true;
+            $errorFname = "First Name must be string.<br>";
+        }
+
+        if(empty($fname)){
+            $error = true;
+            $errorFname= "First Name must be inputted <br>";
+        }else{
+            $error = false;
+        }
+
+        if (!preg_match("/^[a-zA-Z]+$/", $lname)) {
+            $error = true;
+            $errorLname = "Last Name must be string.<br>";
+        }
+
+        if(empty($lname)){
+            $error = true;
+            $errorLname = "Last Name must be inputted <br>";
+        }else{
+            $error = false;
+        }
+
+        if(empty($age)){
+            $error = true;
+            $errorAge = "Age must be inputted.<br>";
+        }else{
+            $error = false;
+        }
+        
+        if (is_numeric($age)) {
+            $error = false;
+        }else{
+            $error = true;
+            $errorAge = "Invalid Age. <br>";
+        }
+
+        if(isset($_POST["Gender"])){
+            $gender = $_POST["Gender"];
+        }else{
+            $gender = "";
+        }
+
+        if (empty($gender)) {
+            $error = true;
+            $errorGender = "Pick Gender";
+        }
+         
+        $pattern = '/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/';
+
+        if (!preg_match($pattern, $email)) {
+            $error = true;
+            $errorEmail = "Email must be valid.<br>";
+        }
+
+        if (!preg_match("/[A-Z]/", $password)) {
+            $error = true;
+            $errorPass = "Password must contain at least one capital letter.<br>";
+        }
+
+        if (!preg_match("/[^a-zA-Z0-9]/", $password)){ 
+            $error = true;
+            $errorPass = "Password must contain at least one special character.<br>";
+        }
+
+        if (empty($password)) {
+            $error = true;
+            $errorPass = "Enter Password";
+        }
+
+        if($cpassword !== $password){
+            $error = true;
+            $errorCpass = "Password not match";
+        }
+
+        if (empty($cpassword)) {
+            $error = true;
+            $errorCpass = "Re-enter Password";
+        }
+
+        if($error === false){
+
+            $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user_fname, user_lname, user_age, user_gender, user_email, user_password) VALUES ('$fname' , '$lname','$age', '$gender ', '$email', '$hashedpassword')";
+            $result = $connection->query($sql);
+             $fname = "";
+            $lname = "";
+            $age = "";
+            $gender = "";
+            $email = "";
+            $password = "";
+            $cpassword = "";
+        }
+
+
+} 
 
 ?>
 
-<link rel="stylesheet" href="styles/register.css">
+<link rel="stylesheet" href="styles/myRegister.css">
 
 <div id = "formContainer">
 
-    <form action="" method = "post">
+    <form action="" method = "POST">
 
         <label for="">First Name:</label><br>
-        <input type="text" id = "nameInput"><br>
-        <label for="" id = "fnameError">ERROR</label><br>
+        <input type="text" id = "nameInput" name = "fname" value = <?php echo $fname ?>><br>
+        <label for="" id = "fnameError"><?php echo $errorFname ?></label><br>
         <label for="">Last Name:</label><br>
-        <input type="text" id = "lastName"><br>
-        <label for="" id = "lnameError">ERROR</label><br>
+        <input type="text" id = "lastName" name = "lname" value = <?php echo $lname ?>><br>
+        <label for="" id = "lnameError"><?php echo $errorLname ?></label><br>
         <label for="">Age:</label><br>
-        <input type="text" id = "ageInput"><br>
-        <label for="" id = "ageError">ERROR</label><br><br>
+        <input type="text" id = "ageInput" name = "age" value = <?php echo $age ?>><br>
+        <label for="" id = "ageError"><?php echo $errorAge ?></label><br><br>
         <label for="">Gender:</label><br>
-        <input type="radio">
-        <label for="">Male:</label>
-        <input type="radio">
-        <label for="">Female:</label><br>
-        <label for="" id = "genderError">ERROR</label><br>
+        <input type="radio" name = "Gender" value = "Male">
+        <label for="" >Male:</label>
+        <input type="radio" name = "Gender" value = "Female">
+        <label for="" >Female:</label><br>
+        <label for="" id = "genderError"><?php echo $errorGender ?></label><br>
         <label for="">Email:</label><br>
-        <input type="text"id = "emailInput"><br>
-        <label for="" id = "emailError">ERROR</label><br>
+        <input type="text"id = "emailInput" name = "email" value = <?php echo $email ?>><br>
+        <label for="" id = "emailError"><?php echo $errorEmail ?></label><br>
         <label for="">Password:</label><br>
-        <input type="text"id = "passwordInput"><br>
-        <label for="" id = "passwordError">ERROR</label><br>
+        <input type="text"id = "passwordInput" name = "password" value = <?php echo $password ?>><br>
+        <label for="" id = "passwordError"><?php echo $errorPass ?></label><br>
         <label for="">Confirm Password:</label><br>
-        <input type="text"id = "confirmpasswordInput"><br>
-        <label for="" id = "cpasswordError">ERROR</label><br>
+        <input type="text"id = "confirmpasswordInput" name = "cpassword" value = <?php echo $cpassword ?>><br>
+        <label for="" id = "cpasswordError"><?php echo $errorCpass ?></label><br>
         <button id="button">Submit</button>
 
     </form>
